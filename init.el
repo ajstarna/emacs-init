4fef5d005e2e8d3f7c5c86b0199ce09f55525030
Adam St Arnaud <ajstarna@ualberta.ca>
2024-11-03 10:20:52 -0700

    Update init.el

--- init.el
+++ init.el
@@ -50,7 +50,29 @@ displayed anywhere else."
 ;; https://rust-analyzer.github.io/manual.html#installation
 ;; 1) rustup component add rust-src
 ;; 2) rustup component add rust-analyzer
-(use-package rustic)
+;; Rust
+;;(use-package rustic)
+(use-package rustic
+  :ensure
+  :bind (:map rustic-mode-map
+              ("M-j" . lsp-ui-imenu)
+              ("M-?" . lsp-find-references)
+              ("C-c C-c l" . flycheck-list-errors)
+              ("C-c C-c a" . lsp-execute-code-action)
+              ("C-c C-c r" . lsp-rename)
+              ("C-c C-c q" . lsp-workspace-restart)
+              ("C-c C-c Q" . lsp-workspace-shutdown)
+              ("C-c C-c s" . lsp-rust-analyzer-status))
+  :config
+  ;; uncomment for less flashiness
+  ;; (setq lsp-eldoc-hook nil)
+  ;; (setq lsp-enable-symbol-highlighting nil)
+  ;; (setq lsp-signature-auto-activate nil)
+
+  ;; comment to disable rustfmt on save
+  ;;(setq rustic-format-on-save t)
+)
+
 
 (setq-default indent-tabs-mode t)
 ;; width of 8 in our code base
